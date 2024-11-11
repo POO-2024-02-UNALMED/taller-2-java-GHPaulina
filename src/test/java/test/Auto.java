d315e64aad3fd45472e75ab480e8637078573e1f
GHPaulina <pgomezh@unal.edu.co>
2024-11-11 14:47:37 -0500

    Clase Auto

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,5 +1,40 @@
 package test;
 
 public class Auto {
-
+	String modelo;
+	int precio;
+	Asiento[] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	int cantidadAsientos() {
+		int cantidad = 0;
+		for (int posicion = 0; posicion<asientos.length ; posicion ++) {
+			if(asientos[posicion] != null) {
+				cantidad ++;
+			}
+		}
+		return cantidad; 
+	}
+	
+	String verificarIntegridad() {
+		if (this.registro == this.motor.registro) {
+			for (int posicion = 0; posicion<asientos.length ; posicion ++) {
+				if(asientos[posicion] != null) {
+					if(asientos[posicion].registro != this.registro) {
+						return("Las piezas no son originales");
+					
+					} else {
+							return("Auto original");
+					}
+				}
+			}
+		}
+		return ("Las piezas no son originales");
+	}
 }
+
+
+
