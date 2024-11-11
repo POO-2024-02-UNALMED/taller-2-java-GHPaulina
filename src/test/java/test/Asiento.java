b9c7d247f4cb64e914a4ae3a23c3f855ae2fb24b
GHPaulina <pgomezh@unal.edu.co>
2024-11-11 14:58:24 -0500

    Clase Asiento

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -1,14 +1,13 @@
 package test;
 
 public class Asiento {
-	String color;
-    int precio;
-    int registro;
-    
-    void cambiarColor(String color) {
-        // Usamos equals() para comparar cadenas
-        if (color.equals("rojo") || color.equals("verde") || color.equals("amarillo") || color.equals("negro") || color.equals("blanco")) {
-            this.color = color;  // Cambia el color del objeto actual
-        }        
-    }
+		String color;
+		int precio;
+		int registro;
+
+		void cambiarColor(String color) {
+			if (color == "rojo" || color == "verde" || color == "amarillo" || color == "negro" || color == "blanco"){
+				this.color = color;	
+			}		
+		}
 }
